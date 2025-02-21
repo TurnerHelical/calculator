8acfed5a65723e2abaaac45e7031d81743bf796e
TurnerHelical <hunterde91@gmail.com>
2025-02-21 11:44:35 -0500

    gave the calculator number button functions and got them to display correctly

--- scripts.js
+++ scripts.js
@@ -1,4 +1,158 @@
+//take user input when pressing buttons
+//make the display of the calculator show the entered numbers and operators for the current operation
+//when equal is selected the calculator finds the solution for the given numbers and operators
+    // operators will be add, subtract, multiply, divide, and an equals button
+//the answer is then displayed to the calculator
+// the user can keep putting in new numbers and operators and the calculator will keep the answer 
 
+
+// add eventlistener to buttonContainer
+// set up event delegation to only perform the action of the button clicked
+const buttonContainer = document.querySelector('#buttonContainer');
+const display = document.querySelector('#display');
+
+let displayArray = [];
+
+// write a function to change the display of the calculator and when an operator is selected push the current 
+// display into an array along with the operator pushed
+function equate(e) {
+    let currentDisplay = '';
+    let length = display.textContent.length;
+    let number = e.target.textContent
+    if (number == 'C') {
+        currentDisplay = '';
+        display.textContent = '';
+        displayArray = [];
+    }
+    else {
+        if (displayArray.length > 9){
+            display.textContent = 'ERROR'
+        } else if (length > 10) {
+            display.textContent = 'OVER MAX CHARS'
+         
+        } else if (isNaN(display.textContent) == true) {
+            display.textContent = 'ERROR'
+        
+        } else {
+            display.textContent += `${number}`;
+            currentDisplay = display.textContent;
+            console.log(currentDisplay);
+        }
+    }
+    // } else if(e.target.matches('#2')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#3')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#4')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#5')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#6')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#7')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#8')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#9')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#0')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#decimal')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // } else if (e.target.matches('#clear')) {
+    //     if (displayArray.length > 9){
+    //         display.textContent = 'ERROR'
+    //     } else if (length > 10) {
+    //         display.textContent = 'OVER MAX CHARS'
+    //     } else {
+    //         display.textContent += '1';
+    //         currentDisplay = display.textContent;
+    //         console.log(currentDisplay);
+    //     }
+    // }
+}
+
+buttonContainer.addEventListener('click', equate)
 
 function add(number1,...numbers) {
     let answer = number1;
